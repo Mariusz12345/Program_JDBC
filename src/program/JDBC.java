3d7a8ba9c902dd0812071a27479f0820617fc7d6
Mariusz12345 <przybylowski303@gmail.com>
2015-07-31 11:22:19 +0200

    ProgramJBC1.1

--- src/program/JDBC.java
+++ src/program/JDBC.java
@@ -9,37 +9,35 @@ import java.sql.SQLWarning;
 import java.sql.Statement;
 
 public class JDBC {
-	
-	
+	private final static String url = "jdbc:postgresql://localhost:5432/test";
+	private final static String userBazyDanych = "postgres";
+	private final static String userHaslo = "maniek1";
+	private final static String driver = "org.postgresql.Driver";
 
 	public static void main(String[] args) {
 		// LADOWANIE STEROWNIKA
-				System.out.print("Sprawdzanie sterownika:");
-		        try {
-					Class.forName("org.postgresql.Driver").newInstance();
-				} catch (Exception e) {
-					System.out.println("Blad przy ladowaniu sterownika bazy!");
-					System.exit(1);
-				}
-				System.out.print(" sterownik OK");
-				
-				// LACZENIE Z BAZA
-				System.out.print("\nLaczenie z baza danych:");
-				String baza = "jdbc:postgresql://localhost:5432/test";
-				
-				String user = "postgres";
-				String pass = "maniek1";
-				java.sql.Connection conn = null;
-		        try {
-					conn=DriverManager.getConnection(baza, user, pass);
-					
-				} catch (SQLException e) {
-					System.out.println("Blad przy ladowaniu sterownika bazy!");
-					System.exit(1);
-				}
-				System.out.print(" polaczenie OK\n");
-				
-				
 
-}
+		Connection conn = null;
+		System.out.print("Sprawdzanie sterownika:");
+		try {
+			Class.forName(driver).newInstance();
+		} catch (Exception e) {
+			System.out.println("Blad przy ladowaniu sterownika bazy!");
+			System.exit(1);
+		}
+		System.out.print(" sterownik OK");
+
+		// LACZENIE Z BAZA
+		System.out.print("\nLaczenie z baza danych:");
+
+		try {
+			conn = DriverManager.getConnection(url, userBazyDanych, userHaslo);
+
+		} catch (SQLException e) {
+			System.out.println("Blad przy ladowaniu sterownika bazy!");
+			System.exit(1);
+		}
+		System.out.print(" polaczenie OK\n");
+
+	}
 }
