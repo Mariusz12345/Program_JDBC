9b736bceb5022d1a1f10a379c31bc1e80f59a6c5
Mariusz12345 <przybylowski303@gmail.com>
2015-08-04 06:48:12 +0200

    ProgramJDBC

--- src/program/JDBC.java
+++ src/program/JDBC.java
@@ -2,10 +2,10 @@ package program;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-
 
 public class JDBC {
 	private final static String url = "jdbc:postgresql://localhost:5432/test3";
@@ -13,16 +13,16 @@ public class JDBC {
 	private final static String userHaslo = "maniek1";
 	private final static String driver = "org.postgresql.Driver";
 
-
 	public static void main(String[] args) throws Exception {
 		// LADOWANIE STEROWNIKA
-		
+
 		Connection connetion = null;
-		//Zapytania do bazy danych za pomoca "Statement"
+		// Zapytania do bazy danych za pomoca "Statement"
 		Statement s = null;
 		System.out.print("Sprawdzanie sterownika:");
 		try {
-			Class.forName(driver).newInstance(); // Zaladowanie sterownika do systemu
+			Class.forName(driver).newInstance(); // Zaladowanie sterownika do
+													// systemu
 		} catch (Exception e) {
 			System.out.println("Blad przy ladowaniu sterownika bazy!");
 			System.exit(1);
@@ -41,19 +41,25 @@ public class JDBC {
 		}
 		System.out.print(" polaczenie OK\n");
 
-		
-		//~~~~~~~~~~~~~~~ Dodanie do bazy danych ~~~~~~~~~~~~
+		// ~~~~~~~~~~~~~~~ Dodanie do bazy danych ~~~~~~~~~~~~
 		connetion.setAutoCommit(false);
 		s = connetion.createStatement();
 		connetion.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
-		
-		//String createTableFaktury = "CREATE TABLE faktury(id_faktury INTEGER PRIMARY KEY,nazwaFaktury VARCHAR )";
-		String zapytanie = "INSERT INTO klient (imie, nazwisko) VALUES ('p', 'p')";
+
+		// String createTableFaktury = "CREATE TABLE faktury(id_faktury INTEGER
+		// PRIMARY KEY,nazwaFaktury VARCHAR )";
+		String zapytanie = "INSERT INTO klient (imie, nazwisko) VALUES ('c', 'c')";
 		s.execute(zapytanie);
 		connetion.commit();
+
+		connetion.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+		connetion.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+
+		// ~~~~~~~~~~~~~~ Koniec dodania ~~~~~~~~~~~~~~~~~~~
+
 		
-		//~~~~~~~~~~~~~~ Koniec dodania ~~~~~~~~~~~~~~~~~~~
-	
+		
+
 		// ZAMYKANIE POLACZENIA Z BAZA
 
 		System.out.print("\nZamykanie polaczenia z baza:");
